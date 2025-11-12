d56744cf7f465fb49f1e65075dd5ec3be1eb7ddc
MasahideMori-SimpleAppli <59500584+MasahideMori-SimpleAppli@users.noreply.github.com>
2025-11-12 21:17:53 +0900

    pre v0.0.37. update docs.

--- lib/src/query/util_field.dart
+++ lib/src/query/util_field.dart
@@ -2,12 +2,15 @@
 ///
 /// (ja) DBの内部処理で利用する、フィールドアクセスに関するユーティリティです。
 class UtilField {
+
   /// (en) Functions for accessing nested fields of a dictionary.
+  /// Returns the found value, or None if it doesn't exist.
   ///
   /// (ja) 辞書の、ネストされたフィールドにアクセスするための関数です。
+  /// 見つかった値、または存在しなければ null を返します。
   ///
-  /// * [map] : 探索したいマップ。
-  /// * [path] : "."区切りの探索用パス。user.nameなど。
+  /// * [map] : A map to explore.
+  /// * [path] : A "." separated search path, such as user.name.
   static dynamic getNestedFieldValue(Map<String, dynamic> map, String path) {
     final keys = path.split('.');
     dynamic current = map;
